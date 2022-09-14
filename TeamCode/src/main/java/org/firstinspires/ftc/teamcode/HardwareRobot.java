84c89fa547234613491ee8658f5bfc439e315d5d
Martin <martin.matura123@gmail.com>
2022-09-14 17:06:13 +0200

    setup; copy Odometry; copy autotest modes from last year; copy HardwareRobot; copy BasicDriveMode; copy EasyOpenCVTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareRobot.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class HardwareRobot {
+    public DcMotorEx leftFront = null;
+    public DcMotorEx rightFront = null;
+    public DcMotorEx leftBack = null;
+    public DcMotorEx rightBack = null;
+
+    public DcMotor encR = null;
+    public DcMotor encS = null;
+    public DcMotor encL = null;
+
+    HardwareMap hdwMap = null;
+    private ElapsedTime period = new ElapsedTime();
+
+    public void HardwareMap(){
+
+    }
+
+    public void init(HardwareMap ahwMap){
+        hdwMap = ahwMap;
+        leftFront = hdwMap.get(DcMotorEx.class, "leftFront");
+        rightFront = hdwMap.get(DcMotorEx.class, "rightFront");
+        leftBack = hdwMap.get(DcMotorEx.class, "leftBack");
+        rightBack = hdwMap.get(DcMotorEx.class, "rightBack");
+
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightBack.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/BasicDriveMode.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.HardwareRobot;
+
+@TeleOp(name="Basic Drive Mode", group="Linear Opmode")
+public class BasicDriveMode extends LinearOpMode {
+    HardwareRobot robot = new HardwareRobot();
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode(){
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        robot.init(hardwareMap);
+        waitForStart();
+        runtime.reset();
+
+        while(opModeIsActive()) {
+            double x = gamepad1.left_stick_x;
+            double y = gamepad1.left_stick_y;
+
+            double speedCoe = Range.clip(1 - gamepad1.left_trigger, 0.3, 1.0);
+
+            //needs to be changed to reflect new sideways drive -> left is forward, forward is right, right is back, back is left
+            double iPower = Range.clip((y - x) * speedCoe, -1.0, 1.0);
+            double kPower = Range.clip((y + x) * speedCoe, -1.0, 1.0);
+
+            //i
+            robot.leftFront.setPower(iPower);
+            robot.rightBack.setPower(iPower);
+            //k
+            robot.rightFront.setPower(kPower);
+            robot.leftBack.setPower(kPower);
+        }
+    }
+}
